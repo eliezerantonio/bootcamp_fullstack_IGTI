558389322fd5c8714314123e3effaddeebd007f9
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-31 01:58:37 +0100

    arrow function e templates literias

--- 02-fundamentos/10-javascript moderno/script.js
+++ 02-fundamentos/10-javascript moderno/script.js
@@ -28,5 +28,31 @@ function withLet() {
 
 withLet();
 
-//const na podemos reatribuir valores
+//const nao podemos reatribuir valores
 const a = 1;
+
+function sum(a, b) {
+    return a + b;
+
+}
+const sum2 = function(a, b) { // funcao anonima
+    return a + b;
+}
+
+// arrow function
+
+const sum3 = (a, b) => {
+    return a + b;
+}
+
+//arrow function reduzida, quandot em apenas um comando
+
+
+const sum4 = (a, b) => a + b;
+
+
+//template literais
+
+const name = 'Eliezer'
+const surname = 'Antonio';
+const text1 = `eu sou o ${name} ${surName}`
